38a074ac9ccd196e8b46afa17fe4a70c6f354c5c
anam soomro <anamsoomroed@gmail.com>
2019-12-26 00:34:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,6 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
 end
 
 def alan_kay_is_known_for
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
