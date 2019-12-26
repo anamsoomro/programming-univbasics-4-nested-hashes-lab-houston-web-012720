fb63464d292427c87ef62a798c082b6f5a51cc2b
anam soomro <anamsoomroed@gmail.com>
2019-12-26 00:46:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,9 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["Lisp", "C"]}
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby", 
+      :languages => ["Lisp", "C"]}
 end
 
 def changing_alan
